3e3f6cff5aa065782a4f10fd12e2f7d1c23178a3
moon9109 <hanhannetnet@naver.com>
2021-07-21 23:14:58 +0900

    Add c++ file [최대힙(우선순위큐).cpp]

---
+++ 최대힙(우선순위큐).cpp
@@ -0,0 +1,60 @@
+
+#include<stdio.h> 
+#include <iostream>
+
+#include <vector>
+
+#include <queue>
+
+#include <algorithm>
+
+using namespace std;
+
+ 
+
+
+// 최대 힙 
+// 	부모가 자식보다 크게 구성 
+
+vector<int> v1; 
+
+int main(void)
+
+{ int a;
+  int i; 
+  priority_queue<int> pQ; 
+  
+  while(true){
+  	
+  	scanf("%d", &a); 
+  	if(a==-1) break; 
+  	
+  	if(a==0){
+  		
+  		if(pQ.empty()) printf("-1\n");
+  		else{
+  			v1.push_back(pQ.top());
+  			//printf("%d",pQ.top());
+  			pQ.pop();
+		  }
+  		
+	  }
+	
+	else pQ.push(a); 
+   }
+   
+   for(i=0 ; i<v1.size(); i++){
+   	
+   	printf("%d\n",v1[i]);
+   }
+        return 0;
+
+}
+
+
+
+
+
+
+
+
